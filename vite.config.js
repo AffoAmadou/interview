6796c1166c461312f13060706c3cb3515e3c774e
Amadou Affo <62760390+AffoAmadou@users.noreply.github.com>
2024-08-14 22:24:31 +0200

    redux

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   rollupOptions: {
-    external: ['@reduxjs/toolkit', 'react-redux'],
+    external: [ 'react-redux', '@reduxjs/toolkit'],
   },
 })
